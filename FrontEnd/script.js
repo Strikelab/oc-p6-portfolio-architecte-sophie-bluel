8ae648b7aba70117e66fe1f59ef4eda6fa08af8b
Strikelab <contact@strikelab.fr>
2023-05-17 13:43:43 +0200

    adding sass and categories buttons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,29 +1,46 @@
 //--------------------------------//
-//           CONSTANTS            //
+//           ENV CONSTANTS        //
 //--------------------------------//
-
-//---------environnement-----------//
-//---------API server URL----------//
 
 const API_URL = "http://localhost:5678";
 const WORKS_PATH = "/api/works/";
-
-
-//get works ressources from swagger API
-
-const response = await fetch(`${API_URL}${WORKS_PATH}`);
-let works = await response.json();
-// Transformation des works en JSON
-
-console.log(works);
+const CATEGORIES_PATH = "/api/categories/";
 
 //--------------------------------//
-//      CREATE DOM ELEMENT        //
+//          API REQUESTS          //
+//--------------------------------//
+
+//works
+const worksRequest = await fetch(`${API_URL}${WORKS_PATH}`);
+let works = await worksRequest.json();
+
+//categories
+const categoriesRequest = await fetch(`${API_URL}${CATEGORIES_PATH}`);
+let categories = await categoriesRequest.json();
+
+//--------------------------------//
+//          DOM ELEMENTS          //
 //--------------------------------//
 
 // DOM parent select
 const divGallery = document.querySelector(".gallery");
+const divButtonsContainer = document.querySelector(".filters-container")
+const divPortFolio = document.querySelector(".portfolio");
 
+//generate buttons
+// button "Tous"
+const categorieButton = document.createElement("button");
+categorieButton.innerText = "Tous";
+divButtonsContainer.appendChild(categorieButton);
+
+//categories buttons
+for (const categorie of categories){
+    const categorieButton = document.createElement("button");
+    categorieButton.innerText = categorie.name;
+    divButtonsContainer.appendChild(categorieButton);
+}
+
+// Add works to DOM
 for(const work of works){
 // DOM Childs
 const figure = document.createElement("figure");
