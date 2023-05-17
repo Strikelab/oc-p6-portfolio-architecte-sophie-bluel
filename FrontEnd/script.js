353813b0dee650bb9b1e0576ea5e21bc4ceb1a3c
Strikelab <contact@strikelab.fr>
2023-05-17 17:07:41 +0200

    update webpage display with filter buttons

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -11,12 +11,24 @@ const CATEGORIES_PATH = "/api/categories/";
 //--------------------------------//
 
 //works
-const worksRequest = await fetch(`${API_URL}${WORKS_PATH}`);
-let works = await worksRequest.json();
+
+const works = await fetch(`${API_URL}${WORKS_PATH}`).then((works) =>
+  works.json()
+);
+// alternate way to write this API call :
+// const worksRequest = await fetch(`${API_URL}${WORKS_PATH}`);
+// let works = await worksRequest.json();
 
 //categories
-const categoriesRequest = await fetch(`${API_URL}${CATEGORIES_PATH}`);
-let categories = await categoriesRequest.json();
+const categories = await fetch(`${API_URL}${CATEGORIES_PATH}`).then(
+  (categories) => categories.json()
+);
+console.log(works);
+console.log(categories);
+
+// alternate way to write this API call :
+// const categoriesRequest = await fetch(`${API_URL}${CATEGORIES_PATH}`);
+// let categories = await categoriesRequest.json();
 
 //--------------------------------//
 //          DOM ELEMENTS          //
@@ -24,7 +36,7 @@ let categories = await categoriesRequest.json();
 
 // DOM parent select
 const divGallery = document.querySelector(".gallery");
-const divButtonsContainer = document.querySelector(".filters-container")
+const divButtonsContainer = document.querySelector(".filters-container");
 const divPortFolio = document.querySelector(".portfolio");
 
 //generate buttons
@@ -34,29 +46,36 @@ categorieButton.innerText = "Tous";
 divButtonsContainer.appendChild(categorieButton);
 
 //categories buttons
-for (const categorie of categories){
-    const categorieButton = document.createElement("button");
-    categorieButton.innerText = categorie.name;
-    divButtonsContainer.appendChild(categorieButton);
+for (const categorie of categories) {
+  const categorieButton = document.createElement("button");
+  categorieButton.innerText = categorie.name;
+  divButtonsContainer.appendChild(categorieButton);
 }
+/**
+ * This function takes an array of object as argument and add elements to DOM.
+ * @param {array} works
+ */
+function generateWorks(works) {
+  divGallery.innerHTML = "";
+  // Add works to DOM
+  for (const work of works) {
+    // DOM Childs
+    const figure = document.createElement("figure");
+    const workPicture = document.createElement("img");
+    const workFigCaption = document.createElement("figcaption");
+    // Childs Attributes
+    workPicture.src = work.imageUrl;
+    workPicture.setAttribute("alt", `${work.title}`);
+    workFigCaption.innerText = `${work.title}`;
+    // Add childs to DOM
+    divGallery.appendChild(figure);
+    figure.appendChild(workPicture);
+    figure.appendChild(workFigCaption);
+  }
+}
+//First gallery generation
+generateWorks(works);
 
-// Add works to DOM
-for(const work of works){
-// DOM Childs
-const figure = document.createElement("figure");
-const workPicture = document.createElement("img");
-const workFigCaption = document.createElement("figcaption");
-// Childs Attributes
-workPicture.src = work.imageUrl;
-workPicture.setAttribute("alt", `${work.title}`);
-workFigCaption.innerText = `${work.title}`;
-// Add childs to DOM
-divGallery.appendChild(figure);
-figure.appendChild(workPicture);
-figure.appendChild(workFigCaption);
-};
-
- 
 //--------------------------------//
 //             FILTERS            //
 //--------------------------------//
@@ -65,22 +84,24 @@ figure.appendChild(workFigCaption);
 const buttonsFilter = document.querySelectorAll(".filters-container button");
 
 // add an event listener for each button
-for(let i = 0; i < buttonsFilter.length; i++){
-    const buttonFilter = document.querySelector(`.filters-container button:nth-child(${i+1})`)
-
-    buttonFilter.addEventListener("click", function(){
-        //first button must return works in all categories
-        if(i===0){
-            console.log(works);
-        }
-        //other buttons must return specific works according to categorie
-        else{
-            const worksFiltered = works.filter(function (work) {
-            return work.categoryId === i;
-        });
-        console.log(worksFiltered);
-         }
-    });
-};
-
-
+for (let i = 0; i < buttonsFilter.length; i++) {
+  const buttonFilter = document.querySelector(
+    `.filters-container button:nth-child(${i + 1})`
+  );
+  //define action for each filter button
+  buttonFilter.addEventListener("click", function () {
+    let worksFiltered;
+    //first button must return works in all categories
+    if (i === 0) {
+      worksFiltered = works;
+    }
+    //other buttons must return specific works according to categorie
+    else {
+      worksFiltered = works.filter(function (work) {
+        return work.categoryId === i;
+      });
+    }
+    //and finally call function
+    generateWorks(worksFiltered);
+  });
+}
