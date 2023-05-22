b7969b6022d6e4fa87b7a9ad75570d8ffd047a19
Strikelab <contact@strikelab.fr>
2023-05-22 21:29:43 +0200

    get token from api with login form

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,57 +1,45 @@
 //--------------------------------//
-//           ENV CONSTANTS        //
+//            CONSTANTS           //
 //--------------------------------//
 
+//environment
 const API_URL = "http://localhost:5678";
 const WORKS_PATH = "/api/works/";
 const CATEGORIES_PATH = "/api/categories/";
+const LOGIN_PATH = "/api/users/login";
 
+//others
+const currentPage = window.location.href;
+let currentPageIsIndex = currentPage.includes("index.html");
+let currentPageIsLogin = currentPage.includes("login.html");
+
+//let userDatas;
 //--------------------------------//
 //          API REQUESTS          //
 //--------------------------------//
 
-//works
+//works list
 
 const works = await fetch(`${API_URL}${WORKS_PATH}`).then((works) =>
   works.json()
 );
 
-//categories
+//categories list
 const categories = await fetch(`${API_URL}${CATEGORIES_PATH}`).then(
   (categories) => categories.json()
 );
 
 //--------------------------------//
-//          DOM ELEMENTS          //
+//          FUNCTION              //
 //--------------------------------//
 
-// DOM parent select
-const divGallery = document.querySelector(".gallery");
-const divButtonsContainer = document.querySelector(".filters-container");
-const divPortFolio = document.querySelector(".portfolio");
-
-//generate buttons
-// button "Tous"
-const categorieButton = document.createElement("button");
-categorieButton.innerText = "Tous";
-divButtonsContainer.appendChild(categorieButton);
-
-//categories buttons
-for (const categorie of categories) {
-  const categorieButton = document.createElement("button");
-  categorieButton.innerText = categorie.name;
-  divButtonsContainer.appendChild(categorieButton);
-}
-let buttonFilterActive = document.querySelector(
-  ".filters-container button:nth-child(1)"
-);
-buttonFilterActive.setAttribute("class", "btn-filter-selected");
-
 /**
- * 
+ *
  * This function takes an array of object as argument and add elements to DOM.
+ * and generate portfolio gallery
+ *
  * @param {array} works
- * 
+ *
  */
 
 function generateWorks(works) {
@@ -72,44 +60,126 @@ function generateWorks(works) {
     figure.appendChild(workFigCaption);
   }
 }
-//First gallery generation
-generateWorks(works);
 
 //--------------------------------//
-//             FILTERS            //
+//     DOM ELEMENTS SELECTION     //
 //--------------------------------//
 
-// get the list of filter buttons
-const buttonsFilter = document.querySelectorAll(".filters-container button");
+// DOM parent select
+const divGallery = document.querySelector(".gallery");
+const divButtonsContainer = document.querySelector(".filters-container");
+const sectionPortFolio = document.querySelector("#portfolio");
 
-// add an event listener for each button
-for (let i = 0; i < buttonsFilter.length; i++) {
-  const buttonFilter = document.querySelector(
-    `.filters-container button:nth-child(${i + 1})`
+//--------------------------------//
+// FIRST INDEX PAGE GENERATION    //
+//--------------------------------//
+if (currentPageIsIndex) {
+  //generate buttons
+  // button "Tous"
+  const categorieButton = document.createElement("button");
+  categorieButton.innerText = "Tous";
+  divButtonsContainer.appendChild(categorieButton);
+
+  //categories buttons
+  for (const categorie of categories) {
+    const categorieButton = document.createElement("button");
+    categorieButton.innerText = categorie.name;
+    divButtonsContainer.appendChild(categorieButton);
+  }
+  let buttonFilterActive = document.querySelector(
+    ".filters-container button:nth-child(1)"
   );
-  //define action for each filter button
-  buttonFilter.addEventListener("click", function () {
-    let worksFiltered;
+  buttonFilterActive.setAttribute("class", "btn-filter-selected");
 
-    //deselect and select button to change its style if selected
-    let buttonFilterActive = document.querySelector(".btn-filter-selected");
-    buttonFilterActive.classList.remove("btn-filter-selected");
-    buttonFilter.classList.add("btn-filter-selected");
+  //First gallery generation
+  generateWorks(works);
 
-    //first button must return works in all categories
-    if (i === 0) {
-      worksFiltered = works;
-    }
-    //other buttons must return specific works according to categorie
-    else {
-      worksFiltered = works.filter(function (work) {
-        return work.categoryId === i;
-      });
-    }
-    //and finally refresh gallery by calling function.
-    generateWorks(worksFiltered);
-  });
+  //--------------------------------//
+  //             FILTERS            //
+  //--------------------------------//
+
+  // get the list of filter buttons
+  const buttonsFilter = document.querySelectorAll(".filters-container button");
+
+  // add an event listener for each button
+  for (let i = 0; i < buttonsFilter.length; i++) {
+    const buttonFilter = document.querySelector(
+      `.filters-container button:nth-child(${i + 1})`
+    );
+    //define action for each filter button
+    buttonFilter.addEventListener("click", function () {
+      let worksFiltered;
+
+      //deselect and select button to change its style if selected
+      let buttonFilterActive = document.querySelector(".btn-filter-selected");
+      buttonFilterActive.classList.remove("btn-filter-selected");
+      buttonFilter.classList.add("btn-filter-selected");
+
+      //first button must return works in all categories
+      if (i === 0) {
+        worksFiltered = works;
+      }
+      //other buttons must return specific works according to categorie
+      else {
+        worksFiltered = works.filter(function (work) {
+          return work.categoryId === i;
+        });
+      }
+      //and finally refresh gallery by calling function.
+      generateWorks(worksFiltered);
+    });
+  }
 }
+
+//--------------------------------//
+//             LOGIN              //
+//--------------------------------//
+
+const loginForm = document.getElementById("login__form");
+const emailField = loginForm.elements["login__email"];
+const passwordField = loginForm.elements["login__password"];
+let email;
+let password;
+
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let email = emailField.value;
+  let password = passwordField.value;
+  console.log(
+    `
+    email :  ${email}
+    password : ${password}
+    `
+  );
+  let user = {
+    email: `${email}`,
+    password: `${password}`,
+  };
+  //define request for fetch
+  let request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(user),
+  };
+  
+  fetch(API_URL + LOGIN_PATH, request)
+    //handle promise
+    .then((response) => {
+      //call to the json method to get body in json format
+      return response.json();
+    })
+    //json() method is also async and return promise so
+    // we need to chain it
+    .then((userDatas) => {
+      //destructuring
+      let { userId, token } = userDatas;
+      console.log("userId: " + userId);
+      console.log("token: " + token);
+    });
+});
 
 //--------------------------------//
 //           TESTS SECTION        //
@@ -125,3 +195,38 @@ for (let i = 0; i < buttonsFilter.length; i++) {
 // alternate way to write this API call :
 // const categoriesRequest = await fetch(`${API_URL}${CATEGORIES_PATH}`);
 // let categories = await categoriesRequest.json();
+
+//############################//
+//#       fetch login        #//
+//############################//
+// if (currentPageIsLogin) {
+//   let user = {
+//     email: "sophie.bluel@test.tld",
+//     password: "S0phie",
+//   };
+//   //define request for fetch
+//   let request = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify(user),
+//   };
+
+//   let login = fetch(API_URL + LOGIN_PATH, request)
+//     //handle promise
+//     .then((response) => {
+//       //call to the json method to get body in json format
+//       return response.json();
+//     })
+//     //json() method is also async and return promise so
+//     // we need to chain it
+//     .then((userDatas) => {
+//       //destructuring
+//       let { userId, token } = userDatas;
+//       console.log("userId: " + userId);
+//       console.log("token: " + token);
+//     });
+// }
+//##############################################
