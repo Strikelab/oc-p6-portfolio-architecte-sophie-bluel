99f9bb630244d6fc81481470c1e2bc9d20fe57f0
Strikelab <contact@strikelab.fr>
2023-05-24 00:42:39 +0200

    deal with login form controls

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,5 +1,5 @@
 //--------------------------------//
-//            CONSTANTS           //
+//    CONSTANTS & VARIABLES       //
 //--------------------------------//
 
 //environment
@@ -14,58 +14,109 @@ let currentPageIsIndex = currentPage.includes("index.html");
 let currentPageIsLogin = currentPage.includes("login.html");
 
 //login user
+let mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const currentUserId = window.localStorage.getItem("userId");
 const currentToken = window.localStorage.getItem("token");
 const loggedIn = currentUserId && currentToken ? true : false;
 const loggedOut = !loggedIn;
-console.log(loggedIn);
-console.log(loggedOut);
+
+//others
+let categories;
+let works;
 
 //--------------------------------//
-//          API REQUESTS          //
-//--------------------------------//
-
-//works list
-
-const works = await fetch(`${API_URL}${WORKS_PATH}`).then((works) =>
-  works.json()
-);
-
-//categories list
-const categories = await fetch(`${API_URL}${CATEGORIES_PATH}`).then(
-  (categories) => categories.json()
-);
-
-//--------------------------------//
-//          FUNCTION              //
+//          FUNCTIONS              //
 //--------------------------------//
 
 /**
  *
- * This function takes an array of object as argument and add elements to DOM.
- * and generate portfolio gallery
+ * This function takes an url as argument
+ * it makes API call and return a promise if no error occured
+ * it throw and catch errors if needed
+ * example :
+ * cont myConstant = await callAPI(http://myurl.tld)
+ * myConstant will be an array of objetcs in JSON format
  *
- * @param {array} works
+ * @param {url} works
+ */
+async function callAPI(url) {
+  return fetch(url)
+    .then((response) => {
+      //shorthand for status != 200
+      if (response.ok) {
+        return response.json();
+      }
+      throw Error(response.statusText);
+    })
+    .catch((error) => {
+      console.log(`Une erreur est survenue : ${error.message}`);
+    });
+}
+
+/**
+ *
+ * This function takes an array of object as argument and add elements to DOM.
+ * and generate filters buttons
+ *
+ * @param {array} datas
+ *
+ */
+function generateFiltersButtons(datas) {
+  if (categories) {
+    // default button for all categories
+    const categorieButton = document.createElement("button");
+    categorieButton.innerText = "Tous";
+    divButtonsContainer.appendChild(categorieButton);
+
+    //categories buttons
+    for (const data of datas) {
+      const categorieButton = document.createElement("button");
+      categorieButton.innerText = data.name;
+      divButtonsContainer.appendChild(categorieButton);
+    }
+    let buttonFilterActive = document.querySelector(
+      ".filters-container button:nth-child(1)"
+    );
+    buttonFilterActive.setAttribute("class", "btn-filter-selected");
+  }
+}
+
+/**
+ *
+ * This function takes an array of object as argument.
+ * It add elements to DOM, and generate portfolio gallery
+ *
+ * @param {array} datas
  *
  */
 
-function generateWorks(works) {
-  divGallery.innerHTML = "";
-  // Add works to DOM
-  for (const work of works) {
-    // DOM Childs
-    const figure = document.createElement("figure");
-    const workPicture = document.createElement("img");
-    const workFigCaption = document.createElement("figcaption");
-    // Childs Attributes
-    workPicture.src = work.imageUrl;
-    workPicture.setAttribute("alt", `${work.title}`);
-    workFigCaption.innerText = `${work.title}`;
-    // Add childs to DOM
-    divGallery.appendChild(figure);
-    figure.appendChild(workPicture);
-    figure.appendChild(workFigCaption);
+function generateWorks(datas) {
+  if (works) {
+    divGallery.innerHTML = "";
+    // Add works to DOM
+    for (const data of datas) {
+      // DOM Childs
+      const figure = document.createElement("figure");
+      const workPicture = document.createElement("img");
+      const workFigCaption = document.createElement("figcaption");
+      // Childs Attributes
+      workPicture.src = data.imageUrl;
+      workPicture.setAttribute("alt", `${data.title}`);
+      workFigCaption.innerText = `${data.title}`;
+      // Add childs to DOM
+      divGallery.appendChild(figure);
+      figure.appendChild(workPicture);
+      figure.appendChild(workFigCaption);
+    }
   }
+}
+//--------------------------------//
+//          API REQUESTS          //
+//--------------------------------//
+
+if (currentPageIsIndex) {
+  categories = await callAPI(API_URL + CATEGORIES_PATH);
+  works = await callAPI(API_URL + WORKS_PATH);
 }
 
 //--------------------------------//
@@ -78,18 +129,24 @@ const divGallery = document.querySelector(".gallery");
 const divButtonsContainer = document.querySelector(".filters-container");
 const sectionPortFolio = document.querySelector("#portfolio");
 const navLogin = document.querySelector("nav li:nth-child(3) a");
+const divLoginMessage = document.querySelector("#login__message");
+const divLoginMailMessage = document.querySelector("#login__mail-message");
+const divLoginPasswordMessage = document.querySelector(
+  "#login__password-message"
+);
+const loginButton = document.querySelector("#login__button");
 
 //--------------------------------//
 //         EVENT LISTENER         //
 //--------------------------------//
-navLogin.addEventListener("click", (e) =>{
-if(loggedIn){
-e.preventDefault();
-window.localStorage.removeItem("userId");
-window.localStorage.removeItem("token");
-window.location.href = "./index.html";
-}
-})
+navLogin.addEventListener("click", (e) => {
+  if (loggedIn) {
+    e.preventDefault();
+    window.localStorage.removeItem("userId");
+    window.localStorage.removeItem("token");
+    window.location.href = "./index.html";
+  }
+});
 
 //--------------------------------//
 // FIRST INDEX PAGE GENERATION    //
@@ -112,23 +169,8 @@ if (loggedIn) {
 }
 
 if (currentPageIsIndex) {
-  //generate buttons
-  // button "Tous"
-  const categorieButton = document.createElement("button");
-  categorieButton.innerText = "Tous";
-  divButtonsContainer.appendChild(categorieButton);
-
-  //categories buttons
-  for (const categorie of categories) {
-    const categorieButton = document.createElement("button");
-    categorieButton.innerText = categorie.name;
-    divButtonsContainer.appendChild(categorieButton);
-  }
-  let buttonFilterActive = document.querySelector(
-    ".filters-container button:nth-child(1)"
-  );
-  buttonFilterActive.setAttribute("class", "btn-filter-selected");
-
+  //First Filters buttons generation
+  generateFiltersButtons(categories);
   //First gallery generation
   generateWorks(works);
 
@@ -172,57 +214,112 @@ if (currentPageIsIndex) {
 //--------------------------------//
 //             LOGIN              //
 //--------------------------------//
+
 if (currentPageIsLogin) {
   const loginForm = document.getElementById("login__form");
   const emailField = loginForm.elements["login__email"];
   const passwordField = loginForm.elements["login__password"];
+
   let email;
   let password;
-
+  let emailIsValid = false;
+  let passwordIsEmpty = true;
+  let message;
+  // add events listener to the form
+  //email field
+  emailField.addEventListener("input", (e) => {
+    let userInput = e.target.value;
+    //email format verification
+    if (userInput.match(mailFormat)) {
+      e.preventDefault();
+      divLoginMailMessage.style.color = "green";
+      message = '<i class="fa-solid fa-check"></i>';
+      emailIsValid = true;
+    } else {
+      divLoginMailMessage.style.color = "red";
+      message = `<p>Veuillez entrer une adresse email valide.</p>`;
+    }
+    divLoginMailMessage.innerHTML = `${message}`;
+  });
+  //password field
+  passwordField.addEventListener("input", (e) => {
+    let userInput = e.target.value;
+    // password length verification
+    if (userInput.length <= 0) {
+      divLoginPasswordMessage.style.color = "red";
+      message = "<p>Veuillez saisir un mot de passe</p>";
+      divLoginPasswordMessage.innerHTML = message;
+    } else {
+      passwordIsEmpty = false;
+      divLoginPasswordMessage.innerHTML = "";
+    }
+  });
+  //On submit
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    divLoginMessage.innerHTML = "";
     let email = emailField.value;
     let password = passwordField.value;
-    console.log(
-      `
-    email :  ${email}
-    password : ${password}
-    `
-    );
-    let user = {
-      email: `${email}`,
-      password: `${password}`,
-    };
-    //define request for fetch
-    let request = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify(user),
-    };
+    if (email === "" || !emailIsValid || password === "" || passwordIsEmpty) {
+      divLoginMessage.style.background = "#F8C471 ";
+      divLoginMessage.innerHTML =
+        "<p>Veuillez compléter tous les champs du formulaire.</p>";
+    } else {
+      // body object for API request
+      let user = {
+        email: `${email}`,
+        password: `${password}`,
+      };
+      //define request for fetch
+      let request = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(user),
+      };
 
-    fetch(API_URL + LOGIN_PATH, request)
-      //handle promise
-      .then((response) => {
-        //call to the json method to get body in json format
-        return response.json();
-      })
-      //json() method is also async and return promise so
-      // we need to chain it
-      .then((userDatas) => {
-        //destructuring
-        let { userId, token } = userDatas;
-        console.log("userId: " + userId);
-        console.log("token: " + token);
-        //put userID and token in user's local storage
-        window.localStorage.setItem("token", token);
-        window.localStorage.setItem("userId", userId);
-
-        //redirection
-        window.location.replace("../index.html");
-      });
+      //API request
+      fetch(API_URL + LOGIN_PATH, request)
+        //handle promise
+        .then((response) => {
+          if (response.status === 200) {
+            //call to the json method to get body in json format
+            return response.json();
+            //deal with user 404 not found
+          } else if (response.status === 404) {
+            emailField.focus();
+            throw "<p>Utilisateur inconnu, veuillez vérifier votre e-mail.</p>";
+            //deal with user 401 unauthorized access user exist but password is wrong
+          } else if (response.status === 401) {
+            passwordField.focus();
+            throw "<p>Accès non autorisé, veuillez vérifier votre mot de passe.</p>";
+          } else {
+            //deal with other status codes
+            throw new Error(
+              "Ooops ! une erreur est survenue, veuillez réessayer."
+            );
+          }
+        })
+        //json() method is also async and return promise so
+        // we need to chain it
+        .then((userDatas) => {
+          //destructuring
+          let { userId, token, message, error } = userDatas;
+          if (!message && userId && token) {
+            //put userID and token in user's local storage
+            window.localStorage.setItem("token", token);
+            window.localStorage.setItem("userId", userId);
+            //redirection
+            window.location.replace("../index.html");
+          }
+        })
+        .catch((error) => {
+          divLoginMessage.style.background = "#F8C471 ";
+          divLoginMessage.innerHTML = error;
+        });
+    }
   });
 }
 
@@ -275,3 +372,53 @@ if (currentPageIsLogin) {
 //     });
 // }
 //##############################################
+//--------------------------------//
+//     CONTROLE DE FORMULAIRE     //
+//--------------------------------//
+
+// async function makeRequest(url) {
+//   await fetch(url).then((response) => {
+//     // Shorthand to check for an HTTP 2xx response status.
+//     // See https://fetch.spec.whatwg.org/#dom-response-ok
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     // Raise an exception to reject the promise and trigger the outer .catch() handler.
+//     // By default, an error response status (4xx, 5xx) does NOT cause the promise to reject!
+//     throw Error(response.statusText);
+
+//   })
+//   .then((json)=>{
+
+//     return json;
+//   })
+//   .catch(function(error) {
+//     console.log('Request failed:', error.message);
+//   });
+// }
+// let montest;
+// const test = makeRequest(API_URL + CATEGORIES_PATH,  montest)
+// await console.log(test)
+
+// //works list
+// const works = await fetch(API_URL + WORKS_PATH)
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw Error(response.statusText);
+//   })
+//   .catch((error) => {
+//     console.log(`Une erreur est survenue : ${error.message}`);
+//   });
+// // categories list
+// const categories = await fetch(API_URL + CATEGORIES_PATH)
+//   .then((response) => {
+//     if (response.ok) {
+//       return response.json();
+//     }
+//     throw Error(response.statusText);
+//   })
+//   .catch((error) => {
+//     console.log(`Une erreur est survenue : ${error.message}`);
+//   });
